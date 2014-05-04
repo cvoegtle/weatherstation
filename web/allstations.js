8a483989be0ec3614ba85ebd295b63d59c376553
christian@voegtle.org <christian@voegtle.org>
2014-05-04 19:18:46 +0200

    Kompakteres Layout Fehlermeldung nur einmal anzeigen

--- web/allstations.js
+++ web/allstations.js
@@ -1,176 +1,196 @@
+var WorkingArea = function () {
+  this.area = document.getElementById("working_area");
 
+  this.columns = {
+    timestamp: {
+      caption: "Zeit",
+      style: "time",
+      visible: false,
+      value: []
+    },
+    location: {
+      caption: "Ort",
+      style: "location",
+      visible: false,
+      value: []
+    },
+    temperature: {
+      caption: "Temp.",
+      style: "temperature",
+      visible: false,
+      value: []
+    },
+    humidity: {
+      caption: "Luftf.",
+      style: "humidity",
+      visible: false,
+      value: []
+    },
+    rain: {
+      caption: "Regen 1h",
+      style: "rain",
+      visible: false,
+      value: []
+    },
+    rain_today: {
+      caption: "Regen",
+      style: "rain_today",
+      visible: false,
+      value: []
+    }
+  };
 
-var WorkingArea = function() {
-    this.area = document.getElementById("working_area");
-
-    this.columns = {
-        timestamp: {
-            caption: "Zeit",
-            style: "time",
-            visible: false,
-            value: []
-        },
-        location: {
-            caption: "Ort",
-            style: "location",
-            visible: false,
-            value: []
-        },
-        temperature: {
-            caption: "Temperatur",
-            style: "temperature",
-            visible: false,
-            value: []
-        },
-        humidity: {
-            caption: "Luftfeuchtigkeit",
-            style: "humidity",
-            visible: false,
-            value: []
-        },
-        rain: {
-            caption: "Regen 1h",
-            style: "rain",
-            visible: false,
-            value: []
-        },
-        rain_today: {
-            caption: "Regen heute",
-            style: "rain_today",
-            visible: false,
-            value: []
-        }
-    };
-
-    this.weatherData =  [];
+  this.weatherData = [];
 
 };
 
 WorkingArea.prototype.update = function (newData) {
-    this.weatherData = newData;
-    this.updateColumns();
-    this.area.appendChild(this.createCaption());
-    for (var row = 0; row < this.weatherData.length; row++) {
-        this.area.appendChild(this.createRow(row));
-    }
+  this.weatherData = newData;
+  this.updateColumns();
+  this.area.appendChild(this.createCaption());
+  for (var row = 0; row < this.weatherData.length; row++) {
+    this.area.appendChild(this.createRow(row));
+  }
 };
 
 WorkingArea.prototype.clear = function () {
-    while (this.area.hasChildNodes()) {
-        this.area.removeChild(this.area.lastChild);
-    }
+  while (this.area.hasChildNodes()) {
+    this.area.removeChild(this.area.lastChild);
+  }
+};
+
+WorkingArea.prototype.reportProblem = function(problemDescription) {
+  var errorLine = document.getElementById("errorLine");
+  if (errorLine != undefined) {
+    errorLine.innerHTML = problemDescription;
+  } else {
+    errorLine = this.createErrorLine(problemDescription);
+    errorLine.id = "errorLine";
+    this.prependChild(this.area, errorLine);
+  }
 };
 
 WorkingArea.prototype.updateColumns = function () {
-    for (var i in this.columns) {
-        this.columns[i].visible = false;
-        this.columns[i].value = [];
-    }
+  for (var i in this.columns) {
+    this.columns[i].visible = false;
+    this.columns[i].value = [];
+  }
 
-    for (i in this.weatherData) {
-        this.columns.timestamp.value[i] = getTimeFractionAsString(this.weatherData[i].timestamp);
-        this.columns.location.value[i] = new Location(this.weatherData[i]);
-        this.columns.temperature.value[i] = this.weatherData[i].temperature + " °C";
-        this.columns.humidity.value[i] = this.weatherData[i].humidity + " %";
-        this.columns.rain.value[i] = getOptionalNumber(this.weatherData[i].rain, "l");
-        this.columns.rain_today.value[i] = getOptionalNumber(this.weatherData[i].rain_today, "l");
-    }
+  for (i in this.weatherData) {
+    this.columns.timestamp.value[i] = getTimeFractionAsString(this.weatherData[i].timestamp);
+    this.columns.location.value[i] = new Location(this.weatherData[i]);
+    this.columns.temperature.value[i] = this.weatherData[i].temperature + " °C";
+    this.columns.humidity.value[i] = this.weatherData[i].humidity + " %";
+    this.columns.rain.value[i] = getOptionalNumber(this.weatherData[i].rain, "l");
+    this.columns.rain_today.value[i] = getOptionalNumber(this.weatherData[i].rain_today, "l");
+  }
 
-    for (i in this.columns) {
-        this.columns[i].visible = this.detectFilledColumn(this.columns[i].value);
-    }
+  for (i in this.columns) {
+    this.columns[i].visible = this.detectFilledColumn(this.columns[i].value);
+  }
 };
 
 WorkingArea.prototype.detectFilledColumn = function (value) {
-    var filled = false;
-    for (var i in value) {
-        if (value[i] !== "") {
-            filled = true;
-        } else {
-            value[i] = "&nbsp;"
-        }
+  var filled = false;
+  for (var i in value) {
+    if (value[i] !== "") {
+      filled = true;
+    } else {
+      value[i] = "&nbsp;"
     }
-    return filled;
+  }
+  return filled;
+};
+
+WorkingArea.prototype.createErrorLine = function (errorMessage) {
+  var errorLine = document.createElement("div");
+  errorLine.className = "error";
+  errorLine.id = "errorLine";
+  errorLine.innerHTML = errorMessage;
+  return errorLine;
 };
 
 WorkingArea.prototype.createCaption = function () {
-    var caption = document.createElement("div");
-    caption.className = "caption";
+  var caption = document.createElement("div");
+  caption.className = "caption";
 
-    for (var i in this.columns) {
-        var element = this.createWeatherElement(this.columns[i].caption, this.columns[i].style);
-        this.appendChild(caption, this.columns[i].visible, element);
-    }
+  for (var i in this.columns) {
+    var element = this.createWeatherElement(this.columns[i].caption, this.columns[i].style);
+    this.appendChild(caption, this.columns[i].visible, element);
+  }
 
-    return caption;
+  return caption;
 };
 
 WorkingArea.prototype.createRow = function (rowNumber) {
-    var row = document.createElement("div");
-    row.className = "row";
+  var row = document.createElement("div");
+  row.className = "row";
 
-    for (var i in this.columns) {
-        var element = this.createWeatherSpan(this.columns[i].value[rowNumber], this.columns[i].style);
-        this.appendChild(row, this.columns[i].visible, element);
-    }
-    return row;
+  for (var i in this.columns) {
+    var element = this.createWeatherSpan(this.columns[i].value[rowNumber], this.columns[i].style);
+    this.appendChild(row, this.columns[i].visible, element);
+  }
+  return row;
 };
 
 
 WorkingArea.prototype.appendChild = function (parent, visible, child) {
-    if (visible) {
-        parent.appendChild(child);
-    }
+  if (visible) {
+    parent.appendChild(child);
+  }
+};
+
+WorkingArea.prototype.prependChild = function (parent, child) {
+  parent.insertBefore(child, parent.firstChild);
 };
 
 WorkingArea.prototype.createWeatherElement = function (text, style) {
-    var element = document.createElement("div");
-    element.className = "column " + style;
-    element.innerHTML = text;
-    return element;
+  var element = document.createElement("div");
+  element.className = "column " + style;
+  element.innerHTML = text;
+  return element;
 };
 
 WorkingArea.prototype.createWeatherSpan = function (value, style) {
-    var element = document.createElement("div");
-    element.className = "column " + style;
+  var element = document.createElement("div");
+  element.className = "column " + style;
 
-    var span = document.createElement("span");
-    span.className = "value";
-    if (value instanceof Location) {
-        var link = document.createElement("a");
-        link.onclick =  function() {
-            window.open(value.forecast, "_blank");
-        };
-        link.innerHTML = value.description;
-        element.appendChild(link);
-    } else {
-        span.innerHTML = value;
-    }
+  var span = document.createElement("span");
+  span.className = "value";
+  if (value instanceof Location) {
+    var link = document.createElement("a");
+    link.onclick = function () {
+      window.open(value.forecast, "_blank");
+    };
+    link.innerHTML = value.description;
+    element.appendChild(link);
+  } else {
+    span.innerHTML = value;
+  }
 
-    element.appendChild(span);
-    return element;
+  element.appendChild(span);
+  return element;
 };
 
-var Location = function(weatherDataset) {
-    this.description = weatherDataset.location;
-    this.forecast = weatherDataset.forecast;
+var Location = function (weatherDataset) {
+  this.description = weatherDataset.location;
+  this.forecast = weatherDataset.forecast;
 };
-
 
 
 function init() {
-    function updateWeatherView(weatherData) {
-        workingArea.clear();
-        workingArea.update(weatherData);
-    }
+  function updateWeatherView(weatherData) {
+    workingArea.clear();
+    workingArea.update(weatherData);
+  }
 
-    function reportConnectionProblem() {
+  function reportConnectionProblem() {
+    workingArea.reportProblem("Verbindung gestört");
+  }
 
-    }
+  var workingArea = new WorkingArea();
 
-    var workingArea = new WorkingArea();
-
-    fetchWeatherData(updateWeatherView, reportConnectionProblem, "all");
-    window.setInterval(fetchWeatherData, 3 * 60 * 1000, updateWeatherView, reportConnectionProblem, "all");
+  fetchWeatherData(updateWeatherView, reportConnectionProblem, "all");
+  window.setInterval(fetchWeatherData, 3 * 60 * 1000, updateWeatherView, reportConnectionProblem, "all");
 }
 
