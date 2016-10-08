c4ecae27eebdea1887c380110f5a74bb825f4efa
christian@voegtle.org <christian@voegtle.org>
2016-10-08 15:13:03 +0200

    Wetter-Daten im tmp Verzeichnis speichern ist keine gute Idee, da tmp beim booten geleert wird Empfänger zurücksetzen, falls wir ungültige Wetter-Datensätze verschicken

--- src/scripts/log_weather_solar.sh
+++ src/scripts/log_weather_solar.sh
@@ -4,30 +4,33 @@
 echo URL: $1
 echo Location: $2
 echo Secret: $3
-#register driver for weather logger
-# modprobe usbserial || echo "couldnt modprobe usbserial"
-# insmod cp210x.ko   || echo "couldnt insmod cp210x.ko"
-
+wetter=/home/pi/wetter
 # Loop forever to read data from USB-WDE1
 
 while true
 do
   socat /dev/ttyUSB0,b9600 STDOUT | \
   while read line
-    do
-      if [[ "${line%%;*}" == '$1' ]] ; then
-        timestamp=`date -Iseconds`
-	watt=`wget http://192.168.199.113/a -q -T 10 -O - |grep Watt|sed s/\ Watt//|sed s/\,/\./|sed s/^\ //`
-	kwh=`wget http://192.168.199.113/a -q -T 10 -O - |grep kWh|sed s/\ kWh//|sed s/^\ //`
-        tmp=`echo "${line};${timestamp};${watt};${kwh}"`
-        echo $tmp >>/tmp/sendbuffer.txt
-	echo $tmp >/tmp/last_weather_watt.txt
-#	wget --post-file=/tmp/sendbuffer.txt -O /tmp/wetter_response.txt http://instantwetter.appspot.com/weatherstation/upload?location=instant\&secret=$3
-        wget --post-file=/tmp/sendbuffer.txt -O /tmp/wetter_response.txt http://$1/weatherstation/upload?location=$2\&secret=$3
-        rc=$?
-        if [[ $rc == 0 ]] ; then
-          rm /tmp/sendbuffer.txt
-        fi
+  do
+    if [[ "${line%%;*}" == '$1' ]] ; then
+      timestamp=`date -Iseconds`
+  	  watt=`wget http://192.168.199.113/a -q -T 10 -O - |grep Watt|sed s/\ Watt//|sed s/\,/\./|sed s/^\ //`
+	  kwh=`wget http://192.168.199.113/a -q -T 10 -O - |grep kWh|sed s/\ kWh//|sed s/^\ //`
+      tmp=`echo "${line};${timestamp};${watt};${kwh}"`
+      echo $tmp >>${wetter}/sendbuffer.txt
+      wget --post-file=${wetter}/sendbuffer.txt -O ${wetter}/wetter_response.txt http://$1/weatherstation/upload?location=$2\&secret=$3
+      rc=$?
+      if [[ $rc == 0 ]] ; then
+        rm ${wetter}/sendbuffer.txt
       fi
-    done
+      read response < ${wetter}/wetter_response.txt 
+      echo response = $response
+      if [[ $response == 'IGNORED' ]] ; then
+        echo reset receiver
+        break 
+      fi
+    fi
+  done
+  echo restart socat
+  sleep 10
 done
