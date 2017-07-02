08e0c9a4f6e144a9fac2fb82e3fa07510ed8524f
christian@voegtle.org <christian@voegtle.org>
2017-07-02 15:22:41 +0200

    falls mehrere USB-Geräte vorhanden sind, dann eins nach dem anderen probieren

--- src/scripts/log_weather.sh
+++ src/scripts/log_weather.sh
@@ -12,34 +12,38 @@ wetter=/home/pi/wetter
 
 while true
 do
-  usbdevice=`ls /dev/ttyUSB*`
-  socat -T3600 ${usbdevice},b9600 STDOUT | \
-  while read line
+  usbdevices=$(ls /dev/ttyUSB* | tr " " "\n")
+  for device in $usbdevices
   do
-    if [[ "${line%%;*}" == '$1' ]] ; then
-      timestamp=`date -Iseconds`
-      tmp=`echo "${line};${timestamp}"`
-      echo $tmp >>${wetter}/sendbuffer.txt
-      linecount=$(wc -l < "${wetter}/sendbuffer.txt")
-      if [[ $(($linecount % $frequency)) == 0 ]] ; then
-        wget --timeout=90 --post-file=${wetter}/sendbuffer.txt -O ${wetter}/wetter_response.txt http://$1/weatherstation/upload?location=$2\&secret=$3
-        rc=$?
-        if [[ $rc == 0 ]] ; then
-          rm ${wetter}/sendbuffer.txt
-        else
-          endtime=`date -Iseconds`
-          echo start: $timestamp, end: $endtime, rc: $rc >>${wetter}/error.log
-        fi
-        read response < ${wetter}/wetter_response.txt
-        echo response = $response
-        if [[ $response != 'ACK' ]] ; then
-          echo reset receiver
-          break
+    echo connect to device ${device}
+    socat -T3600 ${device},b9600 STDOUT | \
+    while read line
+    do
+      if [[ "${line%%;*}" == '$1' ]] ; then
+        timestamp=`date -Iseconds`
+        tmp=`echo "${line};${timestamp}"`
+        echo $tmp >>${wetter}/sendbuffer.txt
+        linecount=$(wc -l < "${wetter}/sendbuffer.txt")
+        if [[ $(($linecount % $frequency)) == 0 ]] ; then
+          wget --timeout=90 --post-file=${wetter}/sendbuffer.txt -O ${wetter}/wetter_response.txt http://$1/weatherstation/upload?location=$2\&secret=$3
+          rc=$?
+          if [[ $rc == 0 ]] ; then
+            rm ${wetter}/sendbuffer.txt
+          else
+            endtime=`date -Iseconds`
+            echo start: $timestamp, end: $endtime, rc: $rc >>${wetter}/error.log
+          fi
+          read response < ${wetter}/wetter_response.txt
+          echo response = $response
+          if [[ $response != 'ACK' ]] ; then
+            echo reset receiver
+            break
+          fi
         fi
       fi
-    fi
+    done
   done
   restarttime=`date -Iseconds`
-  echo $restarttime restart socat rc: $? >>${wetter}/error.log
+  echo $restarttime restart socat $? >>${wetter}/error.log
   sleep 10
 done
