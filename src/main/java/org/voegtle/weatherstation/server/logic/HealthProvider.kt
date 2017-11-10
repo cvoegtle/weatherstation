e362dca77e3893d57cc1581a1a53b71ef066a878
christian@voegtle.org <christian@voegtle.org>
2017-11-10 20:13:03 +0100

    HealthProvider umstellen auf Kotlin

---
+++ src/main/java/org/voegtle/weatherstation/server/logic/HealthProvider.kt
@@ -0,0 +1,47 @@
+package org.voegtle.weatherstation.server.logic
+
+import org.voegtle.weatherstation.server.persistence.HealthDTO
+import org.voegtle.weatherstation.server.persistence.PersistenceManager
+import org.voegtle.weatherstation.server.persistence.entities.LocationProperties
+import org.voegtle.weatherstation.server.util.DateUtil
+import java.util.Date
+import java.util.HashMap
+import java.util.logging.Logger
+import javax.cache.Cache
+import javax.cache.CacheManager
+
+class HealthProvider(private val pm: PersistenceManager, locationProperties: LocationProperties) {
+  private val HEALTH = "health"
+  private val cache: Cache
+
+  private val dateUtil: DateUtil = locationProperties.dateUtil
+
+  init {
+    val cacheFactory = CacheManager.getInstance().cacheFactory
+    val properties = HashMap<String, HealthDTO>()
+    cache = cacheFactory.createCache(properties)
+  }
+
+  fun get(): HealthDTO {
+    val today = dateUtil.today
+    val health: HealthDTO? = cache[HEALTH] as HealthDTO
+    if (health == null || isOutdated(health, today)) {
+      return pm.selectHealth(today).toDTO()
+    }
+    return health
+  }
+
+  operator fun get(day: Date): HealthDTO {
+    return pm.selectHealth(day).toDTO()
+  }
+
+  fun update(health: HealthDTO) {
+    cache.put(HEALTH, health)
+    pm.makePersistant(health)
+  }
+
+  private fun isOutdated(health: HealthDTO, today: Date): Boolean {
+    return today.after(health.day)
+  }
+
+}
