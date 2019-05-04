6a35061b8b3b42742e8b02b17a008af09766d988
christian@voegtle.org <christian@voegtle.org>
2019-05-04 21:24:46 +0200

    Weiteres Sheet als Image-Quelle konfigurieren

--- src/main/java/org/voegtle/weatherstation/server/image/ImageServers.kt
+++ src/main/java/org/voegtle/weatherstation/server/image/ImageServers.kt
@@ -11,6 +11,7 @@ class ImageServers {
     servers.put(0, "https://docs.google.com/spreadsheet/oimg?key=0AnsQlmDoHHbKdFVvS1VEMUp6c3FkcElibFhWUGpramc")
     servers.put(1, "https://docs.google.com/spreadsheets/d/1ahkm9SDTqjYcsLgKIH5yjmqlAh6dKxgfIrZA5Dt9L3o/pubchart")
     servers.put(2, "https://docs.google.com/spreadsheets/d/1eWZ9CoA004HtqDuZWaoeUrhytwkmzCZ9pRlxxw_2gO8/pubchart")
+    servers.put(3, "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ16knPd20F_Z33W_m_bgMudQCwl0cndKxouzl1PtffZJhK9fIxeJXZETsaMEPC3BtCp38mNCeYTEr9/pubchart")
   }
 
   operator fun get(id: Int?): String {
