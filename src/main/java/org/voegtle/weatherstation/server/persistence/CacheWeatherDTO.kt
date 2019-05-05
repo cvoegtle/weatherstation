7b7e554e4ed6960eee0d6c5735d2fdd20c0c8628
christian@voegtle.org <christian@voegtle.org>
2019-05-05 18:15:51 +0200

    Nullwerte für Regen nicht übertragen

--- src/main/java/org/voegtle/weatherstation/server/persistence/CacheWeatherDTO.kt
+++ src/main/java/org/voegtle/weatherstation/server/persistence/CacheWeatherDTO.kt
@@ -1,23 +1,25 @@
 package org.voegtle.weatherstation.server.persistence
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import java.util.Date
 
-data class CacheWeatherDTO (  var id: String,
-                         var time: Date,
-                         var localTime: String,
-                         var location: String,
-                         var locationShort: String,
-                         var temperature: Float,
-                         var humidity: Float? = null,
-                         var barometer: Float? = null,
-                         var solarradiation: Float? = null,
-                         var UV: Float? = null,
-                         var latitude: Float,
-                         var longitude: Float,
-                         var insideTemperature: Float? = null,
-                         var insideHumidity: Float? = null,
-                         var raining: Boolean = false,
-                         var rainLastHour: Float? = null,
-                         var rainToday: Float? = null,
-                         var windspeed: Float? = null,
-                         var forecast: String? = null)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class CacheWeatherDTO(var id: String,
+                           var time: Date,
+                           var localTime: String,
+                           var location: String,
+                           var locationShort: String,
+                           var temperature: Float,
+                           var humidity: Float? = null,
+                           var barometer: Float? = null,
+                           var solarradiation: Float? = null,
+                           var UV: Float? = null,
+                           var latitude: Float,
+                           var longitude: Float,
+                           var insideTemperature: Float? = null,
+                           var insideHumidity: Float? = null,
+                           var raining: Boolean = false,
+                           var rainLastHour: Float? = null,
+                           var rainToday: Float? = null,
+                           var windspeed: Float? = null,
+                           var forecast: String? = null)
