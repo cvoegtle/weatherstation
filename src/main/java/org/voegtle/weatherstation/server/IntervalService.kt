9bf7f254dd1672f1b3ab4bb0b5a9789fdfc685d1
christian@voegtle.org <christian@voegtle.org>
2020-06-02 22:44:21 +0200

    Nur jeden zweiten Request bearbeiten, um Appengine Kosten zu vermeiden

--- src/main/java/org/voegtle/weatherstation/server/IntervalService.kt
+++ src/main/java/org/voegtle/weatherstation/server/IntervalService.kt
@@ -8,9 +8,15 @@ import org.voegtle.weatherstation.server.logic.WeatherDataForwarder
 import org.voegtle.weatherstation.server.logic.WeatherDataSmoother
 import org.voegtle.weatherstation.server.util.parseUtcDate
 import org.voegtle.weatherstation.server.weewx.WeewxDataSet
+import java.time.Duration
+import java.time.LocalDateTime
 import java.util.logging.Logger
 
 @RestController class IntervalService : AbstractWeewxService(Logger.getLogger("IntervalService")) {
+  companion object {
+    var timeOfLastRequest = LocalDateTime.now()
+    val minimumIntervall = Duration.ofSeconds(90)
+  }
 
   @GetMapping("/weatherstation/interval")
   fun receive(@RequestParam ID: String,
@@ -29,16 +35,31 @@ import java.util.logging.Logger
               @RequestParam indoortemp: Float?,
               @RequestParam indoorhumidity: Float?): String {
     validateReceivedRequest(fetchLocationProperties(), ID, PASSWORD)
-    val dataset = WeewxDataSet(time = parseUtcDate(dateutc), temperature = temp, humidity = humidity, barometer = barometer, dailyRain = 10*dailyrain,
-                               rain = 10*rain, UV = UV, solarRadiation = solarradiation, windDirection = winddir, windSpeed = windspeed,
-                               windGust = windgust, indoorTemperature = indoortemp, indoorHumidity = indoorhumidity)
+    if (hasEnoughTimeElapsedSinceLastRequest()) {
+      val dataset = WeewxDataSet(time = parseUtcDate(dateutc), temperature = temp, humidity = humidity, barometer = barometer,
+                                 dailyRain = 10 * dailyrain,
+                                 rain = 10 * rain, UV = UV, solarRadiation = solarradiation, windDirection = winddir, windSpeed = windspeed,
+                                 windGust = windgust, indoorTemperature = indoortemp, indoorHumidity = indoorhumidity)
 
-    val locationProperties = fetchLocationProperties()
-    pm.makePersistant(dataset)
-    WeatherDataSmoother(pm, locationProperties.dateUtil).smoothWeatherData()
-    WeatherDataAggregator(pm, locationProperties.dateUtil).aggregateWeatherData()
-    WeatherDataForwarder(pm, locationProperties).forwardLastDataset()
-    return "OK"
+      val locationProperties = fetchLocationProperties()
+      pm.makePersistant(dataset)
+      WeatherDataSmoother(pm, locationProperties.dateUtil).smoothWeatherData()
+      WeatherDataAggregator(pm, locationProperties.dateUtil).aggregateWeatherData()
+      WeatherDataForwarder(pm, locationProperties).forwardLastDataset()
+      return "OK"
+    }
+    return "VALID-IGNORED"
   }
 
+  fun hasEnoughTimeElapsedSinceLastRequest(): Boolean {
+    val currentTime = LocalDateTime.now()
+    val elapsedTime = Duration.between(timeOfLastRequest, currentTime)
+    return if (elapsedTime > minimumIntervall) {
+      timeOfLastRequest = currentTime
+      true
+    } else {
+      false
+    }
+
+  }
 }
