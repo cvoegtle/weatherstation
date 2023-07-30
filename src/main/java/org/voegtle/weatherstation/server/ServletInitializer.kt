73452ebd3e5b72d669fc9fd61693ddcec9c2ad2c
christian@voegtle.org <christian@voegtle.org>
2023-07-30 20:08:38 +0200

    Include Spring Boot

---
+++ src/main/java/org/voegtle/weatherstation/server/ServletInitializer.kt
@@ -0,0 +1,19 @@
+import com.apple.eawt.Application
+import com.googlecode.objectify.ObjectifyService
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.voegtle.weatherstation.server.image.Image
+import org.voegtle.weatherstation.server.persistence.entities.*
+
+class ServletInitializer : SpringBootServletInitializer() {
+    override fun configure(builder: SpringApplicationBuilder): SpringApplicationBuilder {
+        ObjectifyService.register(AggregatedWeatherDataSet::class.java)
+        ObjectifyService.register(Contact::class.java)
+        ObjectifyService.register(Health::class.java)
+        ObjectifyService.register(LocationProperties::class.java)
+        ObjectifyService.register(SmoothedWeatherDataSet::class.java)
+        ObjectifyService.register(Image::class.java)
+
+        return builder.sources(Application::class.java)
+    }
+}
