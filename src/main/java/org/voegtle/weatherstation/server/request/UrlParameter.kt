423c39402087cf1ef9dff28f03647fd354a4c539
christian@voegtle.org <christian@voegtle.org>
2024-04-03 10:27:19 +0200

    Url Parameter Ext ausbauen

--- src/main/java/org/voegtle/weatherstation/server/request/UrlParameter.kt
+++ src/main/java/org/voegtle/weatherstation/server/request/UrlParameter.kt
@@ -10,7 +10,6 @@ open class UrlParameter(request: HttpServletRequest, dateUtil: DateUtil,
   private val PARAM_BEGIN = "begin"
   private val PARAM_END = "end"
   private val PARAM_SECRET = "secret"
-  private val PARAM_EXTENDED = "ext"
   private val PARAM_TYPE = "type"
   private val PARAM_LOCAL_TIMEZONE = "local_timezone"
 
@@ -20,7 +19,4 @@ open class UrlParameter(request: HttpServletRequest, dateUtil: DateUtil,
   val begin: Date? = getUrlParameterDate(PARAM_BEGIN, isLocalTimezone)
   val end: Date? = getUrlParameterDate(PARAM_END, isLocalTimezone)
   val secret: String? = getUrlParameter(PARAM_SECRET)
-
-  val isExtended: Boolean = getUrlParameterBoolean(PARAM_EXTENDED)
-
-}
+                        }
