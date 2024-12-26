756e098404f29e799237bd131e219c9f1affc5d7
christian@voegtle.org <christian@voegtle.org>
2024-12-26 16:52:00 +0100

    Teststation

--- src/main/java/org/voegtle/weatherstation/server/AbstractWeewxService.kt
+++ src/main/java/org/voegtle/weatherstation/server/AbstractWeewxService.kt
@@ -51,5 +51,22 @@ abstract class AbstractWeewxService(val log: Logger) {
     return secretHash == calculateHash
   }
 
+  private fun createLocationProperties(): LocationProperties {
+    val lp = LocationProperties()
+    lp.location = "testwetter"
+    lp.address = "Entwicklungs Station"
+    lp.cityShortcut = "DEV"
+    lp.city = "Oxengasse"
+    lp.weatherForecast = "https://wetterstationen.meteomedia.de/?station=108030&wahl=vorhersage"
+    lp.secretHash = "2fe3974d34634baf28c732f4793724f11e4a0813a84030f962187b3844485ae4"
+    lp.readHash = "a883d58dbbb62d60da3893c9822d19e43bc371d20ccc5bfdb341f2b120eea54c"
+    lp.expectedDataSets = 500
+    lp.expectedRequests = 500
+    lp.timezone = "Europe/Berlin"
+    lp.longitude=7.862207f
+    lp.latitude=48.02387f
+
+    return lp
+  }
 
 }
